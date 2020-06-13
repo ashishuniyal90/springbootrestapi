6121bdc99bd0f2d8ac468e78bd19b4aa3d887ac6
auniyal <auniyal@deloitte.com>
2020-06-13 15:43:57 +0530

    Model Mapper implementation

---
+++ src/main/java/com/stacksimplify/restapi/controllers/UserModelMapperController.java
@@ -0,0 +1,45 @@
+package com.stacksimplify.restapi.controllers;
+
+import java.util.Optional;
+
+import javax.validation.constraints.Min;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.stacksimplify.restapi.dtos.UserMmDto;
+import com.stacksimplify.restapi.entities.User;
+import com.stacksimplify.restapi.exceptions.UserNotFoundException;
+import com.stacksimplify.restapi.services.UserService;
+
+@RestController
+@RequestMapping("/modelmapper/users")
+public class UserModelMapperController {
+
+	// Autowire user service
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	// Get Mapping By ID
+	// PathVariable Annotation
+	@GetMapping("/{id}")
+	public UserMmDto getUserById(@PathVariable("id") @Min(1) Long id) {
+		try {
+			Optional<User> userOptional = userService.getUserById(id);
+			User user = userOptional.get();
+			UserMmDto userMmDto = modelMapper.map(user, UserMmDto.class);
+			return userMmDto;
+		} catch (UserNotFoundException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+		}
+	}
+}
---
+++ src/main/java/com/stacksimplify/restapi/dtos/UserMmDto.java
@@ -0,0 +1,54 @@
+package com.stacksimplify.restapi.dtos;
+
+public class UserMmDto {
+	
+	private Long userID;
+	
+	private String username;
+	
+	private String email;
+
+	/**
+	 * @return the userID
+	 */
+	public Long getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(Long userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
